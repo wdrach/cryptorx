0357482b6924096d81f65ffeca755b896a2490d1
Will Drach <will@bitovi.com>
2021-02-13 23:23:25 -0600

    Initial run of functions and stuff

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # cryptorx
-RxJS + Coinbase Pro + Technical Analysis Operators
+RxJS + Coinbase Pro/Kraken + Technical Analysis Operators
+
+ohlc: Behavior Subject --> Current Price Object (ish)
+  * don't just use the current price, fetch the last 1000 candles or so as well
+  * open:  pluck('open') --> Open Price of Current Period
+  * close: pluck('close')
+  * high:  pluck('high')
+  * low:   pluck('low')
+
+ticker: Just the price (+ historical data)
+
+
+
+delay(x): delayWhen(price.skip(x))
+
+Use an empty subject in place of price, then take that subject and call subject.next(val) a bunch of times with a random time period to run a simulation
+price = new Subject<Price>();
+
+// do stuff
+
+price.next(/* data from coinbase */)
+...
+
+// record results
+
+Operate:
+ - use open/close/high/low as a source
+ - 
+
+Decide:
+ - use zip on the operators as long as they match up
+ - use combineLatest on operators that don't match up
+ - pipes to compare those things
+
+
+Execute
+ - map that takes in a truthy/falsy value (buy/sell)
+ - "trade" that takes a "level of confidence"
+   * x < -1 sell
+   * -1 < x < 1 noop
+   * x > 1 buy
+
---
+++ src/index.ts
@@ -0,0 +1,156 @@
+import { Subject } from 'rxjs';
+import WS from 'ws';
+import axios from 'axios';
+import { time } from 'console';
+
+// TODO - this should be all products
+// https://api.pro.coinbase.com/products
+type CoinbaseProduct = 'BTC-USD';
+type CoinbaseGranularity = 60 | 300 | 900 | 3600 | 21600 | 86400;
+
+const COINBASE_API = 'https://api.pro.coinbase.com';
+
+class CoinbaseProPrice extends Subject<number> {
+  constructor(product: CoinbaseProduct = 'BTC-USD') {
+    const ws = new WS('wss://ws-feed.pro.coinbase.com');
+
+    ws.addEventListener('message', (ev: any) => {
+      const data = JSON.parse(ev.data);
+
+      const currentPrice = parseFloat(data.price);
+
+      if (isNaN(currentPrice)) return;
+
+      this.next(currentPrice);
+    });
+
+    ws.addEventListener('open', () => {
+      ws.send(JSON.stringify({
+        type: 'subscribe',
+        channels: [{name: 'ticker', product_ids: [product]}]
+      }));
+    });
+    super();
+  }
+}
+
+class Candle {
+  time: number;
+  low: number;
+  high: number;
+  open: number;
+  close: number;
+  volume: number;
+
+  constructor(tlhocv: Array<number>) {
+    this.time = tlhocv[0];
+    this.low = tlhocv[1];
+    this.high = tlhocv[2];
+    this.open = tlhocv[3];
+    this.close = tlhocv[4];
+    this.volume = tlhocv[5];
+  }
+
+  toString() {
+    return `=======================
+time:   ${this.time}
+low:    ${this.low}
+high:   ${this.high}
+open:   ${this.open}
+close:  ${this.close}
+volume: ${this.volume}
+=======================`;
+  }
+}
+
+const _fetchCandles = async (product: CoinbaseProduct, prefetch: number, period: CoinbaseGranularity, current: number) => {
+  console.log('making request for: ', current)
+  // go forward one candle's worth
+  const startDate = new Date(current);
+  const startStr = startDate.toISOString()
+
+  current += 300*period*1000
+
+  let cancel = false
+  if (Date.now() <= current) {
+    current = Date.now()
+    cancel = true
+  }
+
+  const endDate = new Date(current)
+  const endStr = endDate.toISOString()
+
+  console.log('start: ' + startDate.getTime());
+  console.log('end: ', endDate.getTime());
+
+
+  // bump cur by 1 more minute before updating so we don't overlap that minute
+  current += 60*1000
+
+  const query = 'start=' + startStr + '&end=' + endStr
+  const data = await axios.get(`${COINBASE_API}/products/${product}/candles?${query}`).catch(err => console.error(err));
+
+  if (data) {
+    const body = data.data;
+    const snapshot = body.reverse().map((bucket: Array<number>) => {
+      return new Candle(bucket);
+    })
+    // TODO - there's a chance we'll hit burst request limits at some point here
+    if (!cancel) snapshot.push(...await _fetchCandles(product, prefetch, period, current));
+
+    return snapshot;
+  }
+
+  return [];
+}
+
+class CoinbaseProCandle extends Subject<Candle> {
+  constructor(product: CoinbaseProduct = 'BTC-USD', prefetch: number = 300, period: CoinbaseGranularity = 60) {
+    _fetchCandles(product, prefetch, period, Date.now() - (prefetch * period * 1000)).then((candles: Array<Candle>) => {
+      for (let candle of candles) {
+        this.next(candle);
+      }
+
+      let lastTimestamp = candles[candles.length - 1].time;
+      const now = Date.now() / 1000;
+      const diff = now - lastTimestamp;
+      let delay = (2 * period) - diff;
+
+      if (delay < 0) delay = 0;
+
+      setTimeout(async () => {
+        const timeoutCandles = await _fetchCandles(product, 2, period, (lastTimestamp + period)*1000);
+        for (let candle of timeoutCandles) {
+          this.next(candle);
+        }
+
+        lastTimestamp = timeoutCandles[timeoutCandles.length - 1].time;
+
+        setInterval(async () => {
+          const intervalCandles = await _fetchCandles(product, 2, period, (lastTimestamp + period)*1000);
+          for (let candle of intervalCandles) {
+            this.next(candle);
+          }
+  
+          lastTimestamp = intervalCandles[intervalCandles.length - 1].time;
+        }, 1000 * period)
+      }, 1000 * delay)
+
+
+    });
+
+    super();
+  }
+}
+
+/*
+const price = new CoinbaseProPrice();
+price.subscribe((priceVal) => console.log(priceVal));
+*/
+
+
+const candles = new CoinbaseProCandle();
+
+candles.subscribe((candle: Candle) => {
+  console.log(candle.time);
+});
